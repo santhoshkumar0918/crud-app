85528cfe89cbafe3ad111e420ca6ce490fb29691
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-10-31 20:22:43 +0530

    added

--- anchor/programs/crud-app/src/lib.rs
+++ anchor/programs/crud-app/src/lib.rs
@@ -1,30 +1,38 @@
 #![allow(clippy::result_large_err)]
 
-use anchor_lang::{accounts::account, prelude::*};
+use anchor_lang::prelude::*;
 
 declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 
 #[program]
 pub mod counter{
-    use anchor_lang::solana_program::message;
+    // use anchor_lang::solana_program::message;
+
 
     use super::*;
 
- pub fn create_journal_entry(ctx : Context<CreateEntry>, title : String, message : String) -> Result<()>{
+ pub fn create_journal_entry(ctx: Context<CreateEntry>, title : String, message : String) -> Result<()>{
        let journal_entry  = &mut ctx.accounts.journal_entry;
        journal_entry.owner = *ctx.accounts.owner.key;
        journal_entry.title = title;
        journal_entry.message = message ;
+       Ok(())
+ }
+
+ pub fn update_jounral_entry(ctx: Context<UpdateEntry>, _title : String, message : String)  -> Result<()>{
+  let journal_entry = &mut ctx.accounts.journal_entry;
+  journal_entry.message = message;
+  Ok(())
  }
   
 }
 
 #[derive(Accounts)]
 #[instruction(title : String)]
-pub struct CreateEntry <'info>{
+pub struct CreateEntry<'info>{
   #[account(
     init,
-    seeds = {title.as_bytes(), owner.key().as_ref()},
+    seeds = [title.as_bytes(), owner.key().as_ref()],
     bump,
     space = 8 + JournalEntryState::INIT_SPACE,
     payer = owner,
@@ -37,6 +45,25 @@ pub struct CreateEntry <'info>{
   pub system_program : Program<'info , System>
 }
 
+
+#[derive(Accounts)]
+#[instruction(title : String)]
+
+pub struct  UpdateEntry<'info>{
+  #[account(
+    mut,
+    seeds = [title.as_bytes(), owner.key().as_ref()],
+    bump,
+    realloc = 8 + JournalEntryState::INIT_SPACE,
+    realloc::payer = owner,
+    realloc::zero = true, 
+  )]
+  pub journal_entry : Account<'info , JournalEntryState>, 
+
+  #[account(mut)]
+  pub owner : Signer<'info>,
+  pub system_program : Program<'info , System>
+}
 #[account]
 #[derive(InitSpace)]
 pub struct  JournalEntryState {
